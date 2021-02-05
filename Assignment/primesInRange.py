645362b5a46d8f211d7afd9ff78ad112498e1025
Raylyn <raylynpaschen@gmail.com>
2021-02-04 22:30:49 -0600

    Source code completed

--- Assignment/primesInRange.py
+++ Assignment/primesInRange.py
@@ -1,10 +1,21 @@
-def primesInRange(lowBound, highBound):   
-    primeList = []
-    for n in range(lowBound, highBound):
-        if n == 1: 
-            continue
+def primesInRange(lowBound, highBound): 
+    prime_list = []
+    
+# Input validation
+    if lowBound < 2:
+        return None
+    if highBound > 1000:
+        return None
+    if not(isinstance(lowBound, int)):
+        return None
+    if not(isinstance(highBound, int)):
+        return None
+    if lowBound > highBound:
+        return None
+    
+    for n in range(lowBound, highBound+1):
         if n == 2:
-            primeList.append(n)
+            prime_list.append(n)
         if n % 2 == 0:
             continue
         if ((n % 3 == 0) & (n != 3)):
@@ -15,7 +26,21 @@ def primesInRange(lowBound, highBound):
             continue
         if ((n % 9 == 0) & (n != 9)):
             continue
+        if ((n % 11 == 0) & (n != 11)):
+            continue
+        if ((n % 13 == 0) & (n != 13)):
+            continue
+        if ((n % 15 == 0) & (n != 15)):
+            continue
+        if ((n % 17 == 0) & (n != 17)):
+            continue
+        if ((n % 19 == 0) & (n != 19)):
+            continue
+        if ((n % 21 == 0) & (n != 21)):
+            continue
+        if ((n % 23 == 0) & (n != 23)):
+            continue
         else:
-            primeList.append(n)
+            prime_list.append(n)
             
-    return primeList
+    return prime_list
